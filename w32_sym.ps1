8131cd4d3cfd09bad2c695eba48c10752782b649
jmctune <17505625+jmctune@users.noreply.github.com>
2024-11-27 20:24:17 -0600

    add symlink for windows terminal settings.

--- w32_sym.ps1
+++ w32_sym.ps1
@@ -1,1 +1,2 @@
 cmd /c mklink /J $env:USERPROFILE\AppData\Local\nvim .config\nvim
+rm $env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json; cmd /c mklink /H $env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json microsoft_windows\windows_terminal\settings.json
